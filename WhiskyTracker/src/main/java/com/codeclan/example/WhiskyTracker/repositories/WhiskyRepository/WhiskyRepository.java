9c44e99e0ce8d8db0963bf2e22daa7a818606384
Rick Symington <rick.symington@gmail.com>
2019-02-27 15:46:54 +0000

    MVP part 1 done both JPA method and Criteria method

--- WhiskyTracker/src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository/WhiskyRepository.java
+++ WhiskyTracker/src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository/WhiskyRepository.java
@@ -3,5 +3,10 @@ package com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
 import com.codeclan.example.WhiskyTracker.models.Whisky;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface WhiskyRepository extends JpaRepository<Whisky, Long>, WhiskyRepositoryCustom {
+    public List<Whisky> findAllWhiskysFromAYear(int year);
+
+    public List<Whisky> getWhiskyByYear(int year);
 }
--- WhiskyTracker/src/test/java/com/codeclan/example/WhiskyTracker/WhiskyTrackerApplicationTests.java
+++ WhiskyTracker/src/test/java/com/codeclan/example/WhiskyTracker/WhiskyTrackerApplicationTests.java
@@ -1,16 +1,40 @@
 package com.codeclan.example.WhiskyTracker;
 
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository.DistilleryRepository;
+import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository.WhiskyRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class WhiskyTrackerApplicationTests {
 
+	@Autowired
+	WhiskyRepository whiskyRepository;
+
+	@Autowired
+	DistilleryRepository distilleryRepository;
+
 	@Test
 	public void contextLoads() {
 	}
+
+	@Test
+	public void canGetWhiskysFromAParticularYear(){
+		List<Whisky> found = whiskyRepository.getWhiskyByYear(2018);
+	}
+
+	@Test
+	public void canGetWhiskysFromAParticularYearV2(){
+		List<Whisky> found = whiskyRepository.findAllWhiskysFromAYear(2018);
+	}
+	
+
 
 }
